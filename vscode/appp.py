4558dde7f71865b4cec56ffbc04780cc4bfa6c22
drpoudel3785 <dharmarajpoudel@hotmail.com>
2025-12-24 14:14:59 +0545

    login with session and redirect bases on the role for user_dashboard or admin_dashboard

--- vscode/appp.py
+++ vscode/appp.py
@@ -23,6 +23,14 @@ serializer = URLSafeTimedSerializer(app.secret_key)
 import mysql.connector
 db = mysql.connector.connect(host="localhost",user="root", password="P@ssw0rd", database="ai2025c",port=3307)
 
+
+@app.after_request
+def disable_cache(response):
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
+
 @app.route( '/')
 def index():
    return render_template("index.html")
@@ -35,9 +43,68 @@ def about():
 def contact():
    return render_template("contact.html")
 
-@app.route( '/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-   return render_template("login.html")
+    msg = ''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        cursor = db.cursor(dictionary=True)
+        cursor.execute(
+            "SELECT * FROM users WHERE username = %s AND status=%s", 
+            (username, 1)
+        )
+        user = cursor.fetchone()
+
+        if user and check_password_hash(user['password'], password):
+            # SESSION
+            session['loggedin'] = True
+            session['id'] = user['id']
+            session['username'] = user['username']
+            session['role'] = user['role']
+
+            # ROLE BASED REDIRECT
+            if user['role'] == 'admin':
+                redirect_url = url_for('admin_dashboard')
+            else:
+                redirect_url = url_for('user_dashboard')
+
+            # COOKIE
+            response = make_response(redirect(redirect_url))
+            response.set_cookie('username', user['username'], max_age=60*60*24*15)
+
+            return response
+        else:
+            msg = 'Invalid username or password'
+
+    return render_template("login.html", msg=msg)
+
+@app.route('/admin/dashboard')
+def admin_dashboard():
+    if 'loggedin' in session and session['role'] == 'admin':
+        cursor = db.cursor(dictionary=True)
+        cursor.execute("SELECT id, username, email, role, status FROM users")
+        users = cursor.fetchall()
+        total_users = len(users)   # COUNT USERS
+        return render_template('admin/admin_dashboard.html', users=users,  total_users=total_users)
+    
+
+    return redirect(url_for('login'))
+
+
+@app.route('/user/dashboard')
+def user_dashboard():
+    if 'loggedin' in session and session['role'] == 'user':
+        return render_template('user/user_dashboard.html')
+    return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    response = make_response(redirect(url_for('login')))
+    #response.delete_cookie('username')
+    return response
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
